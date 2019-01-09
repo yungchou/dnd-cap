41651ec689ba461fc1ffa23568de62c737b2a138
yc2121 <yc2121@live.com>
2019-01-09 09:09:34 -0600

    more

--- yc.cap.super.learner.1.R
+++ yc.cap.super.learner.1.R
@@ -1,10 +1,8 @@
 if (!require('SuperLearner')) install.packages('SuperLearner'); library(SuperLearner)
-if (!require('parallel')) install.packages('parallel'); library(parallel)
+
+if(TRUE){ # skip the following
 
 if (!require('h2o')) install.packages('h2o'); library(h2o)
-
-if (!require('snow')) install.packages('snow'); library(snow)
-if (!require('Rmpi')) install.packages('Rmpi'); library(Rmpi)
 
 if (!require('MASS')) install.packages('MASS'); library(MASS)
 if (!require('cvAUC')) install.packages('cvAUC'); library(cvAUC)
@@ -20,8 +18,6 @@ if (!require('xgboost')) install.packages('xgboost'); library(xgboost)
 if (!require('KernelKnn')) install.packages('KernelKnn'); library(KernelKnn)
 if (!require('RcppArmadillo')) install.packages('RcppArmadillo'); library(RcppArmadillo)
 
-if(TRUE){ # skip the following
-
 }
 
 #listWrappers()   # Available models in SuperLearner
@@ -34,9 +30,9 @@ df <- read.csv('data/capstone.dataimp.csv') # data set with Boruta selected fetu
 df <- df[-1]
 
 # use 500 observations for developing the model
-#df <- df[1:100,]
+df <- df[1:10000,]
 
-do.smote <- FALSE
+do.smote <- TRUE
 
 if(do.smote) {
 
@@ -88,34 +84,35 @@ y.test  <- test[, 27]
 # For diabetes data set, the following algorithms commented out if
 # low or zero coefficients or compatibility issues in previous test runs.
 
-#---------
-# Tuning
-#---------
+#-------------
+# Tuning Grid
+#-------------
 xgboost.custom <- create.Learner('SL.xgboost'
   ,tune=list(
-    ntrees=c(500,1000,2000), max_depth=1:4
-   ,shrinkage=c(0.01,0.1,0.3), minobspernode=c(10,30,50)
+    ntrees=c(500,1000), max_depth=3:4
+   ,shrinkage=c(0.01,0.1,0.3), minobspernode=c(10,30)
    )
-  ,detailed_names = TRUE, name_prefix = "xgb"
+  ,detailed_names = TRUE, name_prefix = 'xgboost'
 )
 #environment(xgboost.custom) <-asNamespace("SuperLearner")
 
 ranger.custom <- create.Learner('SL.ranger'
  ,tune = list(
-    num.trees = c(500,1000,2000)
+    num.trees = c(500,1000)
    ,mtry = floor(sqrt(ncol(x.train))*c(0.5,1,2))
    #,nodesize = c(1,3,5)
    )
- ,detailed_names = TRUE, name_prefix = "ran"
+ ,detailed_names = TRUE, name_prefix = 'ranger'
 )
+
 #environment(ranger.custom) <-asNamespace("SuperLearner")
 
-glmnet.custom <-  create.Learner("SL.glmnet"
+glmnet.custom <-  create.Learner('SL.glmnet'
  ,tune = list(
-   alpha  = seq(0, 100, length.out=100)
-  ,nlambda = seq(0, 100, length.out=100)
+   alpha  = seq(0, 1, length.out=10)  # (0,1)=>(ridge, lasso)
+  ,nlambda = seq(0, 10, length.out=10)
    )
- ,detailed_names = TRUE, name_prefix = "enet"
+ ,detailed_names = TRUE, name_prefix = 'glmnet'
 )
 #'
 #ranger.custom <- function(...) SL.ranger(...,num.trees=1000, mtry=5)
@@ -128,27 +125,34 @@ family   <-  'binomial' #'gaussian'
 nnls     <- 'method.NNLS'   # NNLS-default
 auc      <- 'method.AUC'   # NNLS-default
 nnloglik <- 'method.NNloglik'
+
 SL.algorithm <- c(
-  #'SL.ranger'
   ranger.custom$names  #,c('ranger.custom$names','screen.corP')
+ ,xgboost.custom$names  #,c('xgboost.custom$names','screen.corP')
+)
+
+#SL.algorithm <- list(c('SL.ranger','screen.corP'),c('SL.xgboost','screen.corP'))
+  #'SL.ranger','SL.xgboost'
+ #glmnet.custom$names#,'screen.glmnet'
  #,c('screen.randomForest','screen.randomForest')
  #,'SL.xgboost'
- ,xgboost.custom$names  #,c('xgboost.custom$names','screen.corP')
  #,'SL.glm'
  #,'SL.bayesglm'
-#,c('glmnet.custome$names','screen.glmnet')
  #,c('kernelKnn.custom','screen.corP')
  #,c('SL.nnet','screen.corP')
  #,c('SL.gbm','screen.corP')
  #,SL.treebag'
  #,'SL.svmRadial'
-)
 
 #-------------------------------
 # Multicore/Parallel Processing
 #-------------------------------
 
-nfold <- 10 # Use 10-20 for production
+nfold <- 3 # Use 10-20 for production
+
+if (!require('parallel')) install.packages('parallel'); library(parallel)
+#if (!require('snow')) install.packages('snow'); library(snow)
+#if (!require('Rmpi')) install.packages('Rmpi'); library(Rmpi)
 
 cl <- makeCluster(detectCores()-1)
 
@@ -161,13 +165,14 @@ clusterExport(cl, c( listWrappers()
 
   ,'SL.algorithm'
   ,ranger.custom$names,xgboost.custom$names
-  ,'kernelKnn.custom'
-  ,'glmnet.custom$names'
+  ,glmnet.custom$names
 
   ))
 
 clusterSetRNGStream(cl, iseed=135)
-
+#----------------------
+# Parallel Proccessing
+#----------------------
 system.time({
 
   clusterEvalQ(cl,{
@@ -178,7 +183,6 @@ system.time({
       ,SL.library=SL.algorithm,cvControl=list(V=nfold)
         )
     saveRDS(ensem.nnls, 'ensem.nnls')
-    pred.nnls.train <- predict.SuperLearner(ensem.nnls, x.train)
 
     # AUC
     ensem.auc <- SuperLearner( Y=y.train, X=x.train, verbose=TRUE
@@ -186,7 +190,6 @@ system.time({
       ,SL.library=SL.algorithm,cvControl=list(V=nfold)
       )
     saveRDS(ensem.auc, 'ensem.auc')
-    pred.auc.train <- predict.SuperLearner(ensem.auc, x.train)
 
     # NNLogLik
     ensem.nnloglik <- SuperLearner( Y=y.train, X=x.train, verbose=TRUE
@@ -194,13 +197,41 @@ system.time({
       ,SL.library=SL.algorithm,cvControl=list(V=nfold)
       )
     saveRDS(ensem.nnloglik, 'ensem.nnloglik')
-    pred.nnloglik.train <- predict.SuperLearner(ensem.nnloglik, x.train)
 
   })
 
-  stopCluster(cl)
+})
+
+system.time({
+
+  clusterEvalQ(cl,{
+
+    # NNLS
+    ensem.nnls.cv <- CV.SuperLearner(Y=y.train, X=x.train, verbose=TRUE
+      ,family=family,method=nnls
+      ,SL.library=SL.algorithm,cvControl=list(V=nfold)
+    )
+    saveRDS(ensem.nnls.cv, 'ensem.nnls.cv')
+
+    # AUC
+    ensem.auc.cv <- CV.SuperLearner( Y=y.train, X=x.train, verbose=TRUE
+      ,family=family,method=auc
+      ,SL.library=SL.algorithm,cvControl=list(V=nfold)
+    )
+    saveRDS(ensem.auc.cv, 'ensem.auc.cv')
+
+    # NNLogLik
+    ensem.nnloglik.cv <- CV.SuperLearner( Y=y.train, X=x.train, verbose=TRUE
+      ,family=family,method=nnloglik
+      ,SL.library=SL.algorithm,cvControl=list(V=nfold)
+    )
+    saveRDS(ensem.nnloglik, 'ensem.nnloglik.cv')
+
+  })
 
 })
+
+stopCluster(cl)
 
 #------------------------------------------
 # Read in results form papallel processing
@@ -209,17 +240,24 @@ ensem.nnls <- readRDS('ensem.nnls');ensem.nnls$times;ensem.nnls
 ensem.auc  <- readRDS('ensem.auc');ensem.auc$times;ensem.auc
 ensem.nnloglik <- readRDS('ensem.nnloglik');ensem.nnloglik$times;ensem.nnloglik
 
+compare.risk <- cbind(ensem.nnls$cvRisk,ensem.auc$cvRisk,ensem.nnloglik$cvRisk)
+colnames(compare.risk) <- c('NNLS', 'AUC','NNLogLik');compare.risk
+
+plot(ensem.nnls$cvRisk,las=1,pch=1)
+points(ensem.auc$cvRisk,col='red',pch=2)
+points(ensem.nnloglik$cvRisk,col='blue',pch=3)
+
 #------------
 # PREDICTION
 #------------
-pred.nnls <- predict.SuperLearner(ensem.nnls, x.test, onlySL=TRUE)
-summary(pred.nnls$library.predict);summary(pred.nnls$pred)
+pred.nnls<- predict.SuperLearner(ensem.nnls, x.test, onlySL=TRUE)
+summary(pred.nnls$pred)
 
 pred.auc <- predict.SuperLearner(ensem.auc, x.test, onlySL=TRUE)
-summary(pred.auc$library.predict);summary(pred.auc$pred)
+summary(pred.auc$pred)
 
 pred.nnloglik <- predict.SuperLearner(ensem.nnloglik, x.test, onlySL=TRUE)
-summary(pred.nnloglik$library.predict);summary(pred.nnloglik$pred)
+summary(pred.nnloglik$pred)
 
 #------------------
 # CONSUSION MATRIX
@@ -247,35 +285,81 @@ noquote(cbind(
   )
 ))
 
-#-----------------------------
-# Comparing the three methods
-#-----------------------------
-library(dplyr);library(plotly)
-err <- as.data.frame( cbind(
+#------------------------------------------------
+# COMPARING PREDICTONS MADE BY THE THREE METHODS
+#------------------------------------------------
+pred3 <- as.data.frame( cbind(
    pred.nnls$pred
   ,pred.auc$pred
   ,pred.nnloglik$pred
-));colnames(err) <- c('nnls','auc','nnloglik'
-);err$label <- as.factor(ifelse(y.test == 0,'Not readmitted','Readmitted')
-);colnames(err) <- c('nnls','auc','nnloglik','label')
+));pred3$label <- as.factor(ifelse(y.test == 0,'Not readmitted','Readmitted')
+);pred3$y.test <- y.test
+colnames(pred3) <- c('nnls','auc','nnloglik','label','y.test');pred3
 
-p3d.method <- plot_ly(err
-  ,x = ~err$nnls, y = ~err$auc, z = ~err$nnloglik, color = err$label
+#-----------------
+# 2D Scatter Plot
+#-----------------
+plot(pred3$nnls,las=1,pch=1, main='Comparison of Predictions')
+points(pred3$auc,col='red',pch=2)
+points(pred3$nnloglik,col='blue',pch=3)
+
+if (!require('dplyr')) install.packages('dplyr'); library(dplyr)
+if (!require('plotly')) install.packages('plotly'); library(plotly)
+
+p2d.pred.method <- plot_ly( pred3, type='scatter',mode = 'markers'
+                            ,width=1280 ,height=700 #,margin=5
+  ,x = ~(1:length(pred3$label)), y=~pred3$nnls, name='NNLS' ) %>%
+  add_trace(y = ~pred3$auc, name='AUC') %>%
+  add_trace(y = ~pred3$nnloglik, name='NNLokLik') %>%
+  add_trace(y = ~pred3$y.test, name='Label') %>%
+  layout( title='Comparing Predictions Made by NNLS/AUC/NNLogLik'
+         ,xaxis=list(title='')
+         ,yaxis=list(title='Prediction')
+         ,plot_bgcolor="rgb(230,230,230)"
+  );p2d.pred.method
+
+#-----------------
+# 3D Scatter Plot
+#-----------------
+p3d.pred.method <- plot_ly( pred3
+  ,x = ~pred3$auc, y = ~pred3$nnls, z = ~pred3$nnloglik, color = pred3$label
   ,hoverinfo = 'text'
   ,text = ~paste(
-     'NNLS:\t',round(err$nnls,7)
-    ,'\nAUC:\t', round(err$auc,7)
-    ,'\nNNLogLik: ', round(err$nnloglik,7)
-    ,'\nLabel:\t', err$label)
+    'AUC:\t',round(pred3$auc,7)
+    ,'\nNNLS:\t', round(pred3$nnls,7)
+    ,'\nNNLogLik:\t', round(pred3$nnloglik,7)
+    ,'\nLabel:\t', pred3$label)
   ,colors = c('blue', 'yellow', 'red')
-  ,marker = list( size = 10, opacity = 1
+  ,marker = list(
+    size = 10, opacity = 0.5
     ,line = list( color = 'black', width = 1))
 ) %>% add_markers() %>%
-  layout(title='Comparison of SuperLearner Methods',scene = list(
-    xaxis = list(title = 'NNLS')
-   ,yaxis = list(title = 'AUC')
-   ,zaxis = list(title = 'NNLogLik'))
-   );p3d.method
+  layout( title='Comparing Predictions Made by NNLS/AUC/NNLogLik',scene = list(
+     xaxis = list(
+      title = 'AUC'
+      ,backgroundcolor="rgb(204,204,204)"
+      ,gridcolor="rgb(255, 255, 255)"
+      ,zerolinecolor="rgb(0,0,0)"
+      ,showbackground=TRUE)
+    ,yaxis = list(
+      title = 'NNLS'
+      ,backgroundcolor="rgb(217,217,217)"
+      ,gridcolor="rgb(255, 255, 255)"
+      ,zerolinecolor="rgb(0,0,0)"
+      ,showbackground=TRUE)
+    ,zaxis = list(
+      title = 'NNLogLik'
+      ,backgroundcolor="rgb(230,230,230)"
+      ,gridcolor="rgb(255, 255, 255)"
+      ,zerolinecolor="rgb(0,0,0)"
+      ,showbackground=TRUE)
+    ,camera = list(
+      up=list(x=0, y=0, z=1)
+      ,center=list(x=0, y=0, z=0)
+      ,eye=list(x=2, y=0.4, z=0.25)
+    )
+  )
+  );p3d.pred.method
 
 #---------------------------------------------
 # External Cross-Validation for the Ensembles
@@ -284,7 +368,6 @@ system.time({
   ensem.cv <-  CV.SuperLearner( Y=y.train, X=x.train, verbose=TRUE, V=3
     ,family=SL.family, method=SL.method ,SL.library=SL.algorithm)
 }); ensem.cv
-})
 
 summary(ensem.cv); saveRDS('ensem','capstone.ensem.cv.rds')
 
